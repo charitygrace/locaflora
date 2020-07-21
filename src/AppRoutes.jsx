be6416969ddd4e4bed76788317299998671808dc
Charity Kirk <charity@BigScreen.local>
2020-07-21 14:58:11 -0400

    modifying routing to work on gh-pages

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -19,7 +19,8 @@ class AppRoute extends React.Component {
   render() {
     return(
       <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'} render={props => <Home {...props} plants={this.props.plants} />} />
+        <Route exact path="/" render={props => <Home {...props} plants={this.props.plants} />} />
+        <Route exact path="/locaflora" render={props => <Home {...props} plants={this.props.plants} />} />
         <Route exact path="/plant/:plantSlug" render={props => <Plant {...props} plants={this.props.plants} key={props.match.params.plantSlug} />} />
         <Route component={NoMatch} />
       </Switch>
