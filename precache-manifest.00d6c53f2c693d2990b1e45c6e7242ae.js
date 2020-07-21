bfac0bf8ab6baf78f8966bc9dccb816fdcc3525e
Charity Kirk <charity@BigScreen.local>
2020-07-21 11:33:26 -0400

    Updates

---
+++ precache-manifest.00d6c53f2c693d2990b1e45c6e7242ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f8a0047512a7f3fb499e6f24e45bb3",
+    "url": "/locaflora/index.html"
+  },
+  {
+    "revision": "7ed858e9c6bc71d9c1cb",
+    "url": "/locaflora/static/css/main.f6cad61b.chunk.css"
+  },
+  {
+    "revision": "8c531370c45193ba7da0",
+    "url": "/locaflora/static/js/2.2eace185.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/locaflora/static/js/2.2eace185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed858e9c6bc71d9c1cb",
+    "url": "/locaflora/static/js/main.600f582c.chunk.js"
+  },
+  {
+    "revision": "3becebf58b086cb76782",
+    "url": "/locaflora/static/js/runtime-main.86ab142c.js"
+  },
+  {
+    "revision": "8ede97b72fef669dcf7eccecdd7d145a",
+    "url": "/locaflora/static/media/plantDefaultImage.8ede97b7.svg"
+  }
+]);
